0dda281c1d7d56ec2983454c610dd7cfffa1361d
Annelie Berner <annelieberner@Annelies-MacBook-Air.local>
2016-07-24 16:13:52 +0200

    updated list

--- onetime.js
+++
@@ -1,9 +0,0 @@
-// $.getJSON("https://api.smartcitizen.me/v0/devices/3545", function(data){
-//   console.log(data);
-// })
-var url1 = "https://api.smartcitizen.me/v0/devices/3545/readings?sensor_id=15&rollup=1m&from=2016-07-17&to=2016-07-20";
-// var url3 = "https://api.smartcitizen.me/v0/devices/3545/readings?sensor_id=15&rollup=8h&function=min&from=2016-07-17&to=2016-07-20";
-
-$.getJSON(url1, function(data){
-	console.log(data);
-})
